bc536d597f5272bdafc02e10fb908f78bd0854da
Omar Carrillo <ocarrillo@accessq.com.mx>
2026-04-04 11:33:03 -0500

    fix: guard ce namespace locals when module2 is disabled

--- aws-mk8s-vk8s/aws-ce-site/00-namespace.tf
+++ aws-mk8s-vk8s/aws-ce-site/00-namespace.tf
@@ -15,5 +15,7 @@ data "volterra_namespace" "buytime_existing" {
 }
 
 locals {
-  module2_namespace_ref = var.create_namespace ? volterra_namespace.buytime[0].name : data.volterra_namespace.buytime_existing[0].name
+  module2_namespace_ref = var.create_module2_foundations ? (
+    var.create_namespace ? volterra_namespace.buytime[0].name : data.volterra_namespace.buytime_existing[0].name
+  ) : local.module2_namespace_name
 }
